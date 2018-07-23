077d7dd65afa381c9b50e5a314732e2540d2fc82
Kvathe <kvathe@gmail.com>
2018-07-22 22:00:32 -0600

    Update KappaPad.ino

--- KappaPad/KappaPad.ino
+++ KappaPad/KappaPad.ino
@@ -17,32 +17,32 @@ void setup() {
 }
 
 CapacitiveKey key1 = CapacitiveKey(
-  2,    //Capacitive Send Pin
-  6,    //Capacitive Sense Pin
+  3,    //Capacitive Send Pin
+  7,    //Capacitive Sense Pin
   10,   //LED Pin
   6,    //Capacitive Threshold
   'z',  //Keyboard Key
   255   //LED Brightness (0-255)
 );
 CapacitiveKey key2 = CapacitiveKey(
-  3,    //Capacitive Send Pin
-  7,    //Capacitive Sense Pin
+  2,    //Capacitive Send Pin
+  6,    //Capacitive Sense Pin
   10,   //LED Pin
   5,    //Capacitive Threshold
   'x',  //Keyboard Key
   255   //LED Brightness (0-255)
 );
 CapacitiveKey key3 = CapacitiveKey(
-  A3,    //Capacitive Send Pin
-  15,    //Capacitive Sense Pin
+  A2,    //Capacitive Send Pin
+  14,    //Capacitive Sense Pin
   10,   //LED Pin
   5,    //Capacitive Threshold
   'c',  //Keyboard Key
   255   //LED Brightness (0-255)
 );
 CapacitiveKey key4 = CapacitiveKey(
-  A2,    //Capacitive Send Pin
-  14,    //Capacitive Sense Pin
+  A3,    //Capacitive Send Pin
+  15,    //Capacitive Sense Pin
   10,   //LED Pin
   5,    //Capacitive Threshold
   'v',  //Keyboard Key
