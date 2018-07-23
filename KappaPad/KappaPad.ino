2dfe7d0c678f08897e0e6cd41f2436240d7a673a
Kvathe <kvathe@gmail.com>
2018-07-22 21:18:49 -0600

    Update KappaPad.ino
    now supports 4 keys and basic LED

--- KappaPad/KappaPad.ino
+++ KappaPad/KappaPad.ino
@@ -16,32 +16,54 @@ void setup() {
   pinMode(DISABLE_PIN, INPUT_PULLUP);
 }
 
-CapacitiveKey key0 = CapacitiveKey(
+CapacitiveKey key1 = CapacitiveKey(
   2,    //Capacitive Send Pin
-  7,    //Capacitive Sense Pin
-  6,    //LED Pin
+  6,    //Capacitive Sense Pin
+  10,   //LED Pin
   6,    //Capacitive Threshold
   'z',  //Keyboard Key
   255   //LED Brightness (0-255)
 );
-CapacitiveKey key1 = CapacitiveKey(
-  4,    //Capacitive Send Pin
-  8,    //Capacitive Sense Pin
+CapacitiveKey key2 = CapacitiveKey(
+  3,    //Capacitive Send Pin
+  7,    //Capacitive Sense Pin
   10,   //LED Pin
   5,    //Capacitive Threshold
   'x',  //Keyboard Key
   255   //LED Brightness (0-255)
 );
+CapacitiveKey key3 = CapacitiveKey(
+  4,    //Capacitive Send Pin
+  8,    //Capacitive Sense Pin
+  10,   //LED Pin
+  5,    //Capacitive Threshold
+  'c',  //Keyboard Key
+  255   //LED Brightness (0-255)
+);
+CapacitiveKey key4 = CapacitiveKey(
+  5,    //Capacitive Send Pin
+  9,    //Capacitive Sense Pin
+  10,   //LED Pin
+  5,    //Capacitive Threshold
+  'v',  //Keyboard Key
+  255   //LED Brightness (0-255)
+);
 
 void loop() {
   bool keyboardActive = digitalRead(DISABLE_PIN);
-  key0.keyUpdate(keyboardActive);
   key1.keyUpdate(keyboardActive);
+  key2.keyUpdate(keyboardActive);
+  key3.keyUpdate(keyboardActive);
+  key4.keyUpdate(keyboardActive);
 
   #ifdef SERIAL_OUTPUT
-  Serial.print(key0.sample);
+  Serial.print(key1.sample);
   Serial.print(",");
-  Serial.println(key1.sample);
+  Serial.print(key2.sample);
+  Serial.print(",");
+  Serial.print(key3.sample);
+  Serial.print(",");
+  Serial.print(key4.sample);
   #endif
 }
 
